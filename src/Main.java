56e4a2eacefa6c6723bb3e1cb9ae61d55af53926
Диана <dianamingazova162@gmail.com>
2023-03-21 01:11:36 +0300

    дз11

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int year = 2024;
+        leapYear(year);
+
+
+
+        System.out.println("Задача 2 ");
+        int clientOSS = 1;
+        int yearOS = 2013;
+        softwareVersion(yearOS, clientOSS);
+
+
+
+        System.out.println("Задача 3 ");
+        var deliveryDistance = 95;
+        int deliveryDay =1;
+        int itog = deliveryTime(deliveryDistance, deliveryDay);
+        System.out.println("Для доставки карты потребуется дней: "+itog);
+
+    }
+
+
+    public static void leapYear(int yearNum) {
+        double yearDiv4 = yearNum / 4;
+        if (yearDiv4 % 1 != 0.0)
+            System.out.println(yearNum + " - не високосный год");
+        else if (yearDiv4 % 25 == 0 && yearDiv4 % 100 != 0)
+            System.out.println(yearNum + " - не високосный год");
+        else System.out.println(yearNum + " - високосный год");
+    }
+
+
+    public static void softwareVersion(int yearOSChecking, int clientOSSChecking){
+        if (clientOSSChecking == 0) {
+            if (yearOSChecking >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (yearOSChecking <= 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+
+            }
+            if (clientOSSChecking == 1) {
+                if (yearOSChecking >= 2015) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                } else if (yearOSChecking <= 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+            }
+        }
+
+    }
+
+    public static int deliveryTime( int DayNum, int Distance ) {
+        Distance = 120;
+        DayNum = 1;
+        if (Distance >20 ){
+            DayNum++;
+        }
+        if (Distance >60 ){
+            DayNum++;
+        }
+
+        return DayNum;
+    }
+
+
+
+}
+
